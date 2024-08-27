8c14af09252824b3aea985d2ac30e0dddbbeb8cd
Tristan Suplico <tsuplico772@gmail.com>
2024-08-27 23:37:46 +0800

    [UPDATE] Tutorial

--- app/page.js
+++ app/page.js
@@ -1,7 +1,22 @@
+"use client";
+
 import React from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const Home = () => {
-  return <div>Home</div>;
+  const router = useRouter();
+
+  const navigate = (page) => {
+    router.push(page);
+  };
+
+  return (
+    <section>
+      <h1> useRouter </h1>
+      <button onClick={() => navigate("about")}> Go to about page</button>
+    </section>
+  );
 };
 
 export default Home;
